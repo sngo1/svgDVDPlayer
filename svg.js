ae4c44a21508cc956fccd56d52df79ad959ed007
sngo1 <sngo1@stuy.edu>
2018-03-04 21:36:45 -0500

    Finished assignment

--- svg.js
+++ svg.js
@@ -1,35 +1,13 @@
 // Samantha Ngo
 // Softdev -- Period 7
-// HW07
-// 2018-03-01
+// K#08: Animation Nation
+// 2018-03-04
 
 var pic = document.getElementById("vimage");
 console.log("PIC: ", pic);
 var cl = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
 console.log("cl: ", cl);
-
-/* NOTES & CODE
-cl.setAttribute("cx", "0");
-cl.setAttribute("cy", "0");
-cl.setAttribute("r", "200");
-cl.setAttribute("fill", "purple");
-cl.setAttribute("stroke", "pink");
-console.log("Attribute cx changed to 200");
-
-pic.appendChild(cl);
-console.log("Child appended.")
-
-var change = function(e){
-    pic = this;
-    this.setAttribute("fill","green");
-    console.log("Color changed to green.");
-}
-*/
-
-// For drawing lines
-var lastX = null;
-var lastY = null;
 
 // Clears the board
 var clearBoard = function(){
@@ -47,87 +25,65 @@ var clearBoard = function(){
     return true;
 }
 
-// console.log("clearboard tests:");
-// clearBoard()
-
-// Activate listener for clear button
-eraser = document.getElementById("clear");
-console.log("ERASER: ", eraser);
-eraser.addEventListener("click", clearBoard);
-
-// Draws a new dot and connects it to the last one
+// Draw a dot given x and y coordinates
 var drawDot = function(xCor, yCor){
-    if (!(lastX == null && lastY == null)){
-	drawBlackLine(lastX, lastY, xCor, yCor);
-	cl = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-	cl.setAttribute("cx", String(lastX));
-	cl.setAttribute("cy", String(lastY));
-	cl.setAttribute("r", "20");
-	cl.setAttribute("fill", "pink");
-	cl.setAttribute("stroke", "pink");
-	console.log("Circle drawn.");
-	pic.appendChild(cl);
-    }
     cl = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     cl.setAttribute("cx", String(xCor));
     cl.setAttribute("cy", String(yCor));
     cl.setAttribute("r", "20");
     cl.setAttribute("fill", "pink");
     cl.setAttribute("stroke", "pink");
-    console.log("Circle drawn.");
+    console.log("Circle drawn at: ", xCor, yCor);
     pic.appendChild(cl);
-    console.log("Child appended.");
     console.log("On the board: ", pic.children);
-    lastCor = [xCor, yCor];
-    lastX = xCor;
-    lastY = yCor;
-    return lastCor;
-}
-
-// NOT IN USE
-// Draws a pink line to mask lines overlapping circles; does not compensate for
-// overlaps
-var drawPinkLine = function(startX, startY, endX, endY){
-    line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    line.setAttribute("x1", String(startX));
-    line.setAttribute("y1", String(startY));
-    line.setAttribute("x2", String(endX));
-    line.setAttribute("y2", String(endY));
-    line.setAttribute("stroke", "pink");
-    line.setAttribute("stroke-width", "2");
-    console.log("Dot Connected.");
-    pic.appendChild(line);
-    console.log("Child appended.");
-    console.log("On the board: ", pic.children);
-    lastCor = [endX, endY];
-    return lastCor;
-}
-
-// Draws a black line given start and end coordinates
-var drawBlackLine = function(startX, startY, endX, endY){
-    line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    line.setAttribute("x1", String(startX));
-    line.setAttribute("y1", String(startY));
-    line.setAttribute("x2", String(endX));
-    line.setAttribute("y2", String(endY));
-    line.setAttribute("stroke", "black");
-    line.setAttribute("stroke-width", "2");
-    console.log("Dot Connected.");
-    pic.appendChild(line);
-    console.log("Child appended.");
-    console.log("On the board: ", pic.children);
-    lastCor = [endX, endY];
-    return lastCor;
-}
-
-// Passes the coordinates of the mouse to the drawDot function
-var clicked = function(e){
-    console.log("X: ", e.offsetX);
-    console.log("Y: ", e.offsetY);
-    drawDot(e.offsetX, e.offsetY);
     return true;
 }
 
-// Activates listener for board
-pic.addEventListener("click", clicked);
-console.log(pic.children);
+var requestID;
+
+// Stop animation
+var stopAnim = function(e){
+    console.log("Cancel ID: ", requestID);
+    window.cancelAnimationFrame(requestID);
+    console.log("Animation stopped.");
+    return true;
+}
+
+var xCor = Math.floor(Math.random() * 501);
+var yCor = Math.floor(Math.random() * 501);
+var xAdd = 5;
+var yAdd = 4;
+
+// Increments coordinates of where the ball is drawn -- Motion
+var runDVD = function(e){
+    // Delete last circle drawn -- stop motion animation
+    clearBoard();
+
+    // Every time a coordinate reaches an edge, reverse it to go in the opposite direction
+    if (xCor >= 500 || xCor <= 0){
+	xAdd *= -1;
+    }
+    if (yCor >= 500 || yCor <= 0){
+	yAdd *= -1;
+    }
+
+    // Update coordinates accordingly
+    xCor += xAdd;
+    yCor += yAdd;
+
+    // Draw new circle at coordinate
+    drawDot(xCor, yCor);
+    requestID = window.requestAnimationFrame(runDVD);
+    console.log("REQID: ", requestID);
+    return true;
+}
+
+// Activate listener for start button
+startButton = document.getElementById("start");
+console.log("Start: ", startButton);
+startButton.addEventListener("click", runDVD);
+
+// Activate listener for stop button
+stopButton = document.getElementById("stop");
+console.log("Stop: ", stopButton);
+stopButton.addEventListener("click", stopAnim);
